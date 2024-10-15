b0e4db05b7b6fb55e923fdfe769f33f36ac628a9
aayush1303 <aayushpanwar5177@gmail.com>
2024-10-15 19:33:57 +0530

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and is designed to meet the requirements of an evaluation assignment.
+
+## Assignment Overview
+
+Thank you for taking the time to read the evaluation assignment. The goal is to create a responsive layout that closely resembles the provided design, using the following specifications:
+
+1. All your code should be inside a div with the ID “assignment”.
+2. Use **Tailwind CSS** for styling.
+3. Include **icons** in your design. Assignments without icons will not be evaluated.
+4. Implement a rotation effect: When the mouse hovers over the box below the icon, the box should rotate 360 degrees. For animations, use a simple JavaScript animation library (GSAP is recommended).
+5. After completing the assignment, please host it on **GitHub Pages** (https://pages.github.com/) and share the publicly accessible URL.
 
 ## Available Scripts
 
@@ -8,7 +18,7 @@ In the project directory, you can run:
 
 ### `npm start`
 
-Runs the app in the development mode.\
+Runs the app in development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
@@ -16,7 +26,7 @@ You may also see any lint errors in the console.
 
 ### `npm test`
 
-Launches the test runner in the interactive watch mode.\
+Launches the test runner in interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
@@ -24,7 +34,7 @@ See the section about [running tests](https://facebook.github.io/create-react-ap
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
+The build is minified, and the filenames include hashes.\
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
@@ -35,9 +45,9 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 
 If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc.) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point, you're on your own.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+You don't have to ever use `eject`. The curated feature set is suitable for small and medium deployments, and you shouldn't feel obligated to use this feature. However, we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
 ## Learn More
 
@@ -47,24 +57,24 @@ To learn React, check out the [React documentation](https://reactjs.org/).
 
 ### Code Splitting
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+This section has moved here: [Code Splitting](https://facebook.github.io/create-react-app/docs/code-splitting).
 
 ### Analyzing the Bundle Size
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+This section has moved here: [Analyzing the Bundle Size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size).
 
 ### Making a Progressive Web App
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+This section has moved here: [Making a Progressive Web App](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app).
 
 ### Advanced Configuration
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+This section has moved here: [Advanced Configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration).
 
 ### Deployment
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+This section has moved here: [Deployment](https://facebook.github.io/create-react-app/docs/deployment).
 
 ### `npm run build` fails to minify
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This section has moved here: [Troubleshooting](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useRef } from 'react';
+import { FaTools, FaTint, FaWater, FaHotTub } from 'react-icons/fa';
+import { MdArrowForwardIos } from 'react-icons/md';
+import { gsap } from 'gsap';
 
 function App() {
+  const cardRefs = useRef([]);
+
+  // Function to rotate the card on hovering on the card container below the circular icon container
+  const handleMouseEnter = (index) => {
+    gsap.to(cardRefs.current[index], { rotationY: 360, duration: 1 });
+  };
+
+  const handleMouseLeave = (index) => {
+    gsap.to(cardRefs.current[index], { rotationY: 0, duration: 1 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="assignment" className='flex items-center min-h-screen justify-center '>
+      <div className='diagonal-split-background py-10 w-[80%]'>
+        <h2 className="text-5xl font-semibold ml-[75px] text-white mb-[80px] ">OUR SERVICES</h2>
+        <div className="flex flex-wrap justify-center gap-10">
+
+          {/* Card 1 */}
+          <div className='flex justify-center mb-2'>
+            <div className="w-20 h-20 mt-[-40px] absolute rounded-full bg-purple-100 flex items-center justify-center shadow-md">
+              <FaTools className="text-4xl text-purple-700" />
+            </div>
+            <div
+              className="card-container relative p-6 w-60 shadow-lg overflow-hidden z-10 flex justify-center"
+              onMouseEnter={() => handleMouseEnter(0)}
+              onMouseLeave={() => handleMouseLeave(0)}
+              ref={(el) => (cardRefs.current[0] = el)}
+            >
+              <div className='text-center'>
+                <div className="text-xl font-bold mb-2 mt-10 w-[200px] text-center">Emergency Plumbing Services</div>
+                <p className="text-gray-600 text-sm mb-4 font-medium text-center">
+                  Our plumbers are ready to go 24/7 for emergencies - including nights, weekends, and holidays.
+                </p>
+                <a href="#" className="text-purple-600 font-bold flex text-md items-center gap-1 justify-center ">EXPLORE THIS SERVICE <span className='text-sm'><MdArrowForwardIos/></span></a>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 2 */}
+          <div className='flex justify-center mb-2'>
+            <div className="w-20 h-20 mt-[-40px] absolute rounded-full bg-purple-100 flex items-center justify-center shadow-md">
+              <FaTint className="text-4xl text-purple-700" />
+            </div>
+            <div
+              className="card-container relative p-6 w-60 shadow-lg overflow-hidden z-10 flex justify-center"
+              onMouseEnter={() => handleMouseEnter(1)}
+              onMouseLeave={() => handleMouseLeave(1)}
+              ref={(el) => (cardRefs.current[1] = el)}
+            >
+              <div className='text-center'>
+                <div className="text-xl font-bold mb-2 mt-10 w-[200px] text-center">Plumbing and Drains</div>
+                <p className="text-gray-600 text-sm mb-4 font-medium text-center">
+                  As the largest plumbing and drain service company, we make thousands of repairs every day.
+                </p>
+                <a href="#" className="text-purple-600 font-bold flex text-md items-center gap-1 justify-center">EXPLORE THIS SERVICE <span className='text-sm'><MdArrowForwardIos/></span></a>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 3 */}
+          <div className='flex justify-center'>
+            <div className="w-20 h-20 mt-[-40px] absolute rounded-full bg-purple-100 flex items-center justify-center shadow-md">
+              <FaWater className="text-4xl text-purple-700" />
+            </div>
+            <div
+              className="card-container relative p-6 w-60 shadow-lg overflow-hidden z-10 flex justify-center"
+              onMouseEnter={() => handleMouseEnter(2)}
+              onMouseLeave={() => handleMouseLeave(2)}
+              ref={(el) => (cardRefs.current[2] = el)}
+            >
+              <div className='text-center'>
+                <div className="text-xl font-bold mb-2 mt-10 w-[200px] text-center">Water Damage</div>
+                <p className="text-gray-600 text-sm mb-4 font-medium text-center">
+                  Our teams are equipped with state-of-the-art water extraction and cleanup equipment.
+                </p>
+                <a href="#" className="text-purple-600 font-bold flex text-md items-center gap-1 justify-center">EXPLORE THIS SERVICE <span className='text-sm'><MdArrowForwardIos/></span></a>
+              </div>
+            </div>
+          </div>
+
+          {/* Card 4 */}
+          <div className='flex justify-center'>
+            <div className="w-20 h-20 mt-[-40px] absolute rounded-full bg-purple-100 flex items-center justify-center shadow-md">
+              <FaHotTub className="text-4xl text-purple-700" />
+            </div>
+            <div
+              className="card-container relative p-6 w-60 shadow-lg overflow-hidden z-10 flex justify-center"
+              onMouseEnter={() => handleMouseEnter(3)}
+              onMouseLeave={() => handleMouseLeave(3)}
+              ref={(el) => (cardRefs.current[3] = el)}
+            >
+              <div className='text-center'>
+                <div className="text-xl font-bold mb-2 mt-10 w-[200px] text-center">Water Heaters</div>
+                <p className="text-gray-600 text-sm mb-4 font-medium text-center">
+                  Trust us for repair and replacement of gas, electric, and tankless water heaters.
+                </p>
+                <a href="#" className="text-purple-600 font-bold flex text-md items-center gap-1 justify-center">EXPLORE THIS SERVICE <span className='text-sm'><MdArrowForwardIos/></span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
